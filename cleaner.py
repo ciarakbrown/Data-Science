b36b5638e868f36542554c7723ac63887b1e137c
AniBoja <aniboja26@gmail.com>
2025-03-06 20:09:58 +0000

    Add ARIMA state-space model with Kalman filter for imputation.

--- cleaner.py
+++ cleaner.py
@@ -1,7 +1,15 @@
 from pandas import DataFrame
-from pykalman import KalmanFilter
 import numpy as np
+from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import KNNImputer, IterativeImputer
+from pmdarima import auto_arima
+from statsmodels.tsa.stattools import adfuller
+import statsmodels.api as sm
+
+# Drop columns that have been decided to be ignored
+def base_clean(df: DataFrame, columns):
+    df.drop(columns=columns)
+    pass
 
 # Replace NaNs with the mean of that column that belongs to the
 # target class. May introduce bias and overfit.
@@ -12,28 +20,32 @@ def class_mean_impute(df: DataFrame):
     for col in df.columns[:-1]:
         df[col] = df.groupby(target_column)[col].transform(lambda x: x.fillna(x.mean()))
 
-# Kalman filter
-# Simply fill NaNs with ffill and bfill first
-# Apply Kalman Filter to that filled set
-# Impute the NaNs with the corresponding means from the previous step
-def kalman_fill(df: DataFrame):
-    nan_columns = df.columns[df.isna().all()]
-    df = df.drop(columns=nan_columns)
-    df_temp_fill = df.ffill().bfill()
-    n_dim_obs = df.shape[1]
-    kf = KalmanFilter(
-        initial_state_mean=df_temp_fill.iloc[0].values,
-        n_dim_obs=n_dim_obs,
-        initial_state_covariance=np.eye(n_dim_obs),
-        observation_covariance=np.eye(n_dim_obs),
-        transition_covariance=np.eye(n_dim_obs) * 0.01
+
+def arima_kalman_imputer(ts):
+    nan_mask = ts.isna()
+    ts_clean = ts.dropna()
+    model = auto_arima(
+        ts_clean,
+        seasonal=False,
+        suppress_warnings=True,
+        stepwise=False,
+        information_criterion='aicc',
+        trend='ct',
     )
-    state_means, _ = kf.em(df_temp_fill).smooth(df_temp_fill.values)
-    df_filled = df.copy()
-    for col in range(n_dim_obs):
-        nan_mask = df.iloc[:, col].isna()
-        df_filled.iloc[nan_mask, col] = state_means[nan_mask, col]
-    return df_filled
+    arima_ssm = sm.tsa.statespace.SARIMAX(
+        ts,
+        order=model.order,
+        trend='ct',
+        enforce_stationarity=False,
+        enforce_invertibility=False,
+        initialization="stationary",
+        initial_variance=1e4
+    )
+    result = arima_ssm.fit(disp=False, maxiter=200, method="nm")
+    data_filled = result.get_prediction().predicted_mean
+    ts_filled = ts.copy()
+    ts_filled[nan_mask] = data_filled[nan_mask]
+    return ts_filled
 
 # Regression imputation
 def regression_fill(df: DataFrame):
--- runner.py
+++ runner.py
@@ -2,6 +2,12 @@ import cleaner
 import loader
 
 # Put training set path here
-training_setA = loader.loader("")
-df = cleaner.kalman_fill(training_setA[0])
-print(df.head)
+training_setA = loader.loader("/home/dipl0id/Documents/training_setA/training")
+df = training_setA[0]
+df = df.drop(columns=["EtCO2"])
+for column in df.columns:
+    loc = df.columns.get_loc(column)
+    x = cleaner.arima_kalman_imputer(df.iloc[:,loc])
+    df[column] = x
+    print(loc, " columns completed.")
+print(df)
