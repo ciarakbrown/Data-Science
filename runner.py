280afbe994dbcea143ed6a37c6ec79881a439312
AniBoja <aniboja26@gmail.com>
2025-03-12 19:56:01 +0000

    Add MCMC cleaner. Make IterativeImputer work with dataset and class structure.

--- runner.py
+++ runner.py
@@ -6,8 +6,10 @@ from Removing_missing_features import get_high_missing_features
 # Read path from command line
 parser = argparse.ArgumentParser()
 parser.add_argument('filename')
+parser.add_argument('output')
 args = parser.parse_args()
 path = args.filename
+out = args.output
 
 # Put training set path here
 training_setA = load_data(path)
@@ -19,9 +21,13 @@ balanced_df = undersample_non_sepsis(training_setA, status, missing_data)
 
 # Drop columns with too many missing features
 columns_to_drop = get_high_missing_features(balanced_df, 75)
-demographic_columns = ["Age", "Gender", "Unit1", "Unit2"]
+demographic_columns = ["Unit1", "Unit2"]
 columns_to_drop += demographic_columns
 
 # Create a cleaner object and clean
-cleaner = cl.Cleaner("arima_kalman_cleaner", balanced_df, columns_to_drop, False)
+cleaner = cl.Cleaner("regression_cleaner", balanced_df, columns_to_drop, False)
 cleaner.clean()
+
+for patient in cleaner.cleaned_data:
+    output = out + "/" + patient["patient_id"][0]
+    patient.to_csv(output, sep='|', index=False)
